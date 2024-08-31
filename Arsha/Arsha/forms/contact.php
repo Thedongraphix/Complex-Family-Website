772d85707533f789d2ae99f77336c8881c278345
Thedongraphix <chrisoketchcn@gamil.com>
2024-08-31 21:02:11 +0300

    Updated contact form

--- Arsha/Arsha/forms/contact.php
+++ Arsha/Arsha/forms/contact.php
@@ -1,41 +1,23 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $url = 'https://api.web3forms.com/submit';
+    $postData = $_POST;
+    $ch = curl_init($url);
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+    $response = curl_exec($ch);
+    curl_close($ch);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    $result = json_decode($response, true);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    if ($result['success']) {
+        // Show success message
+        echo "<div class='sent-message'>Your message has been sent. Thank you!</div>";
+    } else {
+        // Show error message
+        echo "<div class='error-message'>There was an error submitting your form. Please try again later.</div>";
+    }
+}
 ?>
